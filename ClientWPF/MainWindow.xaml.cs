1603b6002b6cacaacb677c2e929eac0a862b54d5
Andrew Kang <andrewkang1106@gmail.com>
2022-06-23 11:28:02 -0400

    deleted Client Project. Added ClientWPF project. Inserted Client, main program into ClientWPF

---
+++ ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Grpc.Core;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+                var client = new AccountService.AccountServiceClient(channel);
+                EmployeeName empName = client.GetEmployeeName(new EmployeeNameRequest { EmpId = "1" });
+                if (empName == null || string.IsNullOrWhiteSpace(empName.FirstName) || string.IsNullOrWhiteSpace(empName.LastName))
+                {
+                    Console.WriteLine("Employee not found.");
+                }
+                else
+                {
+                    Console.WriteLine($"The employee name is {empName.FirstName} {empName.LastName}.");
+                }
+                channel.ShutdownAsync().Wait();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }//end of try
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception encountered: {ex}");
+            }//end of catch
+
+
+
+        }//end of MainWindow
+    }//end of public partial class
+
+/*    class Client
+    {
+        static void Main(string[] args)
+        {
+           
+        }//end of main
+    }//end of client*/
+}//end of namespace
